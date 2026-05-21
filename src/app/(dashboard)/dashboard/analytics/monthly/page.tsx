8ccbb20adaa8ac43185aa0a457583137a4a19040
md-zeon <zeon.cse@gmail.com>
2026-05-21 23:30:13 +0600

    feat: add Analytics layout and pages for weekly and monthly views

---
+++ src/app/(dashboard)/dashboard/analytics/monthly/page.tsx
@@ -0,0 +1,5 @@
+const MonthlyPage = () => {
+  return <div>Monthly Page</div>;
+};
+
+export default MonthlyPage;
---
+++ src/app/(dashboard)/dashboard/analytics/weekly/page.tsx
@@ -0,0 +1,5 @@
+const WeeklyPage = () => {
+  return <div>Weekly Page</div>;
+};
+
+export default WeeklyPage;
