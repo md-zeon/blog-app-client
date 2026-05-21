ee3cf55525dd73efc086b05938fcca3cb5f0ed9d
md-zeon <zeon.cse@gmail.com>
2026-05-21 07:54:38 +0600

    feat: update AboutPage to remove simulated loading and error states

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 
-const AboutPage = async () => {
-  await new Promise((resolve) => setTimeout(resolve, 4000));
+// export const dynamic = "force-dynamic"; // this will make the page dynamic and it will not be cached by the server
 
-  throw new Error("Something went wrong in the about page");
+const AboutPage = async () => {
+  // Simulate a delay to show the loading state
+  // await new Promise((resolve) => setTimeout(resolve, 4000));
+
+  // Simulate an error to show the error state
+  // throw new Error("Something went wrong in the about page");
   return <div>About Page</div>;
 };
 
