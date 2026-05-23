053220ed6df7c54a2150dff6e2075ff3731cb81d
md-zeon <zeon.cse@gmail.com>
2026-05-23 10:07:05 +0600

    feat: implement getBlogs action and integrate it into ContactPage for fetching blog data

--- src/app/(common)/contact/page.tsx
+++ src/app/(common)/contact/page.tsx
@@ -1,6 +1,19 @@
+"use client";
+import { getBlogs } from "@/actions/blog.action";
 import React from "react";
 
 const ContactPage = () => {
+  const [data, setData] = React.useState();
+  const [error, setError] = React.useState<string | null>(null);
+
+  console.log(data, error);
+  React.useEffect(() => {
+    (async () => {
+      const { data, error } = await getBlogs();
+      setData(data);
+      setError(error);
+    })();
+  }, []);
   return <div>Contact Page</div>;
 };
 
